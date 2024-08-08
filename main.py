4cdea1b5528b99b307cfcf37a622c8ecc9895669
Ritschwumms <155912571+Ritschwumms@users.noreply.github.com>
2024-08-08 09:05:57 +0200

    Roll up

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import websockets
 import asyncio
 
 all_clients = []
+nicknames = []
 
 async def send_message(message, user):
     for client in all_clients:
@@ -11,7 +12,10 @@ async def new_client_connected(client_socket, path):
     try:
         print("New client connected.")
         all_clients.append(client_socket)
-        await send_message(f"{str(name)} joined.", client_socket)
+        await client_socket.send("NICK")
+        name = await client_socket.recv()
+        nicknames.append(name)
+        await send_message(f"{str(name)} joined.")
 
         while True:
             new_message = await client_socket.recv()
@@ -22,14 +26,15 @@ async def new_client_connected(client_socket, path):
         print("Disconnected.")
         index = all_clients.index(client_socket)
         all_clients.remove(client_socket)
+        nickname = nicknames[index]
+        nicknames.remove(nickname)
+
 
 async def start_server():
     print("Server starting...")
     await websockets.serve(new_client_connected, "", 443)
     print("Server started")
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-loop.run_until_complete(start_server())
-loop.run_forever()
-
+event_loop = asyncio.get_event_loop()
+event_loop.run_until_complete(start_server())
+event_loop.run_forever()
